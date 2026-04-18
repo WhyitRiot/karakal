6ff683b00ef5b54b8498b1ac56ef7a03259dd456
wyatt.t.putnam.mil@army.mil <wyatt.t.putnam.mil@army.mil>
2026-04-17 21:46:57 -0500

    Initial Spring initialize

---
+++ src/main/java/com/ordnance/karakal/KarakalApplication.java
@@ -0,0 +1,13 @@
+package com.ordnance.karakal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarakalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarakalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ordnance/karakal/KarakalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordnance.karakal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarakalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
