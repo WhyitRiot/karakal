336de056020a4e000958cf39accbdb1518e19fbe
wyatt.t.putnam.mil@army.mil <wyatt.t.putnam.mil@army.mil>
2026-05-05 12:52:19 -0500

    Fixed spelling error in site routes

--- frontend/src/utilities/SiteRoutes.tsx
+++ frontend/src/utilities/SiteRoutes.tsx
@@ -5,15 +5,17 @@ import Join from "../pages/Join.tsx";
 import Create from "../pages/Create.tsx";
 import Game from "../pages/Game.tsx";
 import Login from "../pages/Login.tsx";
+import Replay from "../pages/Replay.tsx";
 
 const SiteRoutes = () => {
     return (
         <Routes>
             <Route path={"/"} element={<Login/>}/>
-            <Route path={"/Home"} element={<Home />}/>
+            <Route path={"/home"} element={<Home />}/>
             <Route path={"/join"} element={<Join/>}/>
             <Route path={"/create"} element={<Create/>}/>
             <Route path={"/game"} element={<Game/>}/>
+            <Route path={"/replay"} element={<Replay />}/>
         </Routes>
     );
 };
