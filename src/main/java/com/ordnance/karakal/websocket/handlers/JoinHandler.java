32db096e0e2adbb57f53c5c1a5bf22e7752ae71d
wyatt.t.putnam.mil@army.mil <wyatt.t.putnam.mil@army.mil>
2026-05-06 12:03:35 -0500

    Added check for existing players in a game so current players are not duplicated; Added replay service to track participants

--- src/main/java/com/ordnance/karakal/websocket/handlers/JoinHandler.java
+++ src/main/java/com/ordnance/karakal/websocket/handlers/JoinHandler.java
@@ -1,5 +1,6 @@
 package com.ordnance.karakal.websocket.handlers;
 
+import com.ordnance.karakal.rest.replay.ReplayService;
 import com.ordnance.karakal.websocket.GameService;
 import com.ordnance.karakal.websocket.messages.JoinMessage;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -12,17 +13,20 @@ import java.util.UUID;
 @Component
 public class JoinHandler implements MessageHandler<JoinMessage>{
     private GameService gameService;
+    private ReplayService replayService;
     private SimpMessagingTemplate simpMessagingTemplate;
-    public JoinHandler(GameService gameService, SimpMessagingTemplate simpMessagingTemplate){
+    public JoinHandler(GameService gameService, ReplayService replayService, SimpMessagingTemplate simpMessagingTemplate){
         this.gameService = gameService;
+        this.replayService = replayService;
         this.simpMessagingTemplate = simpMessagingTemplate;
     }
     @Override
     public void handle(JoinMessage message, Principal principal) {
         UUID playerId = UUID.fromString(principal.getName());
-        List<UUID> players = gameService.getAllPlayers(message.gameId);
+        List<UUID> players = this.gameService.getAllPlayers(message.gameId);
         if (!players.contains(playerId)){
-            gameService.addPlayer(message.gameId, message.playerName, UUID.fromString(principal.getName()));
+            this.gameService.addPlayer(message.gameId, message.playerName, UUID.fromString(principal.getName()));
+            this.replayService.addPlayer(UUID.fromString(principal.getName()), message.gameId);
             this.simpMessagingTemplate.convertAndSendToUser(principal.getName(), "/queue/new-player", playerId);
         }
         this.simpMessagingTemplate.convertAndSendToUser(principal.getName(), "/queue/player-state", this.gameService.getPlayerState(message.gameId, playerId));
