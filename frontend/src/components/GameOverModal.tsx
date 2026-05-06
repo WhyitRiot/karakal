fa3d3872fed68be17a41c53bca0b6adcd0112289
wyatt.t.putnam.mil@army.mil <wyatt.t.putnam.mil@army.mil>
2026-05-06 12:08:24 -0500

    Made it look cool; also added function to navigate back to home screen on exit

--- frontend/src/components/GameOverModal.tsx
+++ frontend/src/components/GameOverModal.tsx
@@ -1,5 +1,9 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {GameStateContext} from "../utilities/websocket/GameStateContext.tsx";
+import GameOver from "../assets/GameOver.gif"
+import Back from "../assets/Back.gif"
+import BackHover from "../assets/BackOrange.gif"
+import GifButton from "../components/GifButton.tsx"
 import {useNavigate} from "react-router";
 
 const GameOverModal = ({gameOver} : {gameOver: boolean}) => {
@@ -19,21 +23,23 @@ const GameOverModal = ({gameOver} : {gameOver: boolean}) => {
 
     const handleExit = () => {
         setIsExiting(true);
-        navigate("/");
+        sessionStorage.removeItem("gameId");
+        navigate("/home");
     }
 
     return (
         <div className={`fixed inset-0 ${isVisible ? `z-50` : `-z-1`} flex h-screen justify-center items-center ${isVisible && 'backdrop-blur-md'}`}>
             <div className={"absolute flex flex-col items-center justify-center w-1/2 h-2/3"}>
                 <div
-                      className={`flex flex-col items-center w-full h-full justify-evenly bg-gray-400 rounded-4xl ${isVisible ? (isExiting ? 'animate-fade-out' : 'animate-fade-in') : 'translate-y-full opacity-0'}`}
+                      className={`flex flex-col items-center w-full h-full justify-evenly bg-white shadow-lg drop-shadow-lg rounded-4xl ${isVisible ? (isExiting ? 'animate-fade-out' : 'animate-fade-in') : 'translate-y-full opacity-0'}`}
                       onAnimationEnd={() => {
                           if (isExiting) setIsVisible(false);
                       }}
                 >
-                    <p className={"text-5xl self-center"}>Game Over!</p>
-                    <p className={"text-5xl self-center text-amber-300"}>{leaderboard && `${leaderboard[0].name} is the winner!`}</p>
-                    <div className={"flex flex-col gap-5 w-2/3"}>
+                    {/*<p className={"text-5xl self-center"}>Game Over!</p>*/}
+                    <img src={GameOver} alt="Game Over!"/>
+                    <p className={"text-5xl self-center text-amber-300 font-[Gloria]"}>{leaderboard && `${leaderboard[0].name} is the winner!`}</p>
+                    <div className={"flex flex-col gap-5 w-2/3 font-[Gloria]"}>
                         <p className={"text-4xl self-center"}>Scores</p>
                         <table className={"text-3xl w-full"}>
                             <tbody>
@@ -46,7 +52,7 @@ const GameOverModal = ({gameOver} : {gameOver: boolean}) => {
                             </tbody>
                         </table>
                     </div>
-                    <button onClick={handleExit} className={"text-3xl border rounded p-2 hover:bg-green-400 hover:cursor-pointer"}>Return to Main Menu</button>
+                    <GifButton nonHover={Back} hover={BackHover} type={"button"} click={handleExit} />
                 </div>
             </div>
         </div>
